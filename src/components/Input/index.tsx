6a93d9d1666277aac6b67539ab47b69bd31cb33e
belapferreira <isabelapenhaferreira@gmail.com>
2021-12-30 11:18:50 -0300

    feat: start input component development

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import Container from '../../styles/components/Input';
+
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type: 'top' | 'bottom' | 'single';
+  icon?: JSX.Element;
+}
+
+function Input({ type, placeholder, icon }: IInputProps) {
+  return (
+    <Container type={type}>
+      <input type="text" placeholder={placeholder}/>
+      <div>{icon}</div>
+    </Container>
+  )
+}
+
+export default Input;
